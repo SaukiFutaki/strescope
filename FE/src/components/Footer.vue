f30ea4890b37bc46e0ed212622696c82b99c42fe
unknown <arumjjimo@gmail.com>
2025-06-13 16:42:58 +0800

    update

--- FE/src/components/Footer.vue
+++ FE/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="absolute bottom-0 left-0 right-0 p-4 text-sm text-center text-white">
-    StressCope &copy; 2025 | Built by Team CC25-CF317
+    StressCope &copy; 2025 | Built with ❤️ by Husnul & Frens
   </footer>
 </template>
